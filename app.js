2d549f48982bc0a3a75ba6ce1bed9b914584c0d9
Santino2005 <santinocolombo13@gmail.com>
2024-10-20 15:20:32 -0300

    get push

--- app.js
+++ app.js
@@ -203,17 +203,9 @@ app.get('/pelicula/:id', (req, res) => {
 // Ruta para mostrar la página de un actor específico
 app.get('/actor/:id', (req, res) => {
     const actorId = req.params.id;
-
     // Consulta SQL para obtener las películas en las que participó el actor
-    const query = `
-    SELECT DISTINCT
-      person.person_name as actorName,
-      movie.*
-    FROM movie
-    INNER JOIN movie_cast ON movie.movie_id = movie_cast.movie_id
-    INNER JOIN person ON person.person_id = movie_cast.person_id
-    WHERE movie_cast.person_id = ?;
-  `;
+    const query = 'SELECT DISTINCT person.person_name as actorName, movie.* FROM movie INNER JOIN movie_cast ON movie.movie_id = movie_cast.movie_id INNER JOIN person ON person.person_id = movie_cast.person_id WHERE movie_cast.person_id = ?';
+    ;
 
     // Ejecutar la consulta
     db.all(query, [actorId], (err, movies) => {
@@ -234,15 +226,8 @@ app.get('/director/:id', (req, res) => {
     const directorId = req.params.id;
 
     // Consulta SQL para obtener las películas dirigidas por el director
-    const query = `
-    SELECT DISTINCT
-      person.person_name as directorName,
-      movie.*
-    FROM movie
-    INNER JOIN movie_crew ON movie.movie_id = movie_crew.movie_id
-    INNER JOIN person ON person.person_id = movie_crew.person_id
-    WHERE movie_crew.job = 'Director' AND movie_crew.person_id = ?;
-  `;
+    const query = 'SELECT DISTINCT person.person_name as directorName, movie.* FROM movie INNER JOIN movie_crew ON movie.movie_id = movie_crew.movie_id INNER JOIN person ON person.person_id = movie_crew.person_id WHERE movie_crew.job = '/Director/' AND movie_crew.person_id = ?';
+  ;
 
 
     // console.log('query = ', query)
